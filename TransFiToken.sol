98d850e558db22affc1b7fc72345732edaf511c1
giselitapalacios12 <103265483+giselitapalacios12@users.noreply.github.com>
2025-05-17 07:27:58 -0500

    Update TransFiToken.sol

--- TransFiToken.sol
+++ TransFiToken.sol
@@ -4,7 +4,26 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TransFiToken is ERC20 {
-    constructor() ERC20("TransFi Token", "TFI") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+    uint256 public maxTokensPerWallet = 1000 * (10 ** decimals());
+
+    constructor(uint256 initialSupply) ERC20("TransFi Token", "TFI") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Condición sin afectar el resto del contrato
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        // Solo se aplica si no es una quema (burn) o mint inicial
+        if (to != address(0)) {
+            require(
+                balanceOf(to) + amount <= maxTokensPerWallet,
+                "No puedes tener más de 1000 TFI tokens"
+            );
+        }
     }
 }
